556c7c4d69f823c6e6de43a010d816ef9754d369
pengzhanlee <cdlipengzhan@jd.com>
2017-10-14 11:06:51 +0800

    dev tool

--- src/store.js
+++ src/store.js
@@ -3,40 +3,23 @@ import createSagaMiddleware from "redux-saga";
 import appReducer from "./redux/modules/reducer";
 import initSagas from "./redux/initSagas";
 
-// import {composeWithDevTools} from 'remote-redux-devtools';
-
-
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [
     sagaMiddleware,
 ];
 
-// https://chrome.google.com/webstore/detail/remotedev/faicmgpfiaijcedapokpbdejaodbelph
-// const composeEnhancers = composeWithDevTools({
-//     realtime: process.env.NODE_ENV === 'development',
-// });
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md
+        }) : compose;
 
 const store = createStore(
     appReducer,
-    applyMiddleware(...middleware),
+    composeEnhancers(applyMiddleware(...middleware)),
 );
 
-// const store = createStore(appReducer, composeEnhancers(
-//     applyMiddleware(...middleware),
-// ));
-
 sagaMiddleware.run(initSagas);
-
-
-// if (module.hot) {
-//     // Enable hot module replacement for reducers
-//     module.hot.accept(() => {
-//         const nextRootReducer = require('./redux/modules/reducer').default;
-//         store.replaceReducer(nextRootReducer);
-//     });
-// }
 
 export default store;
